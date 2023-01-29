1e9c75ff77cfaa5bded7ae03ea627a6a88ccb0fa
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-01-29 16:20:54 +0000

    mask_gen.py now working

--- mask_gen.py
+++ mask_gen.py
@@ -1,26 +1,52 @@
+import argparse
+import glob
+import os
+
+from PIL import Image, ImageDraw
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--source_dir', type=str, default='/content/test', help='source')
-parser.add_argument('--txt_output_dir', type=str, default='/content/output', help='txt_output_dir')
-parser.add_argument('--mask_output_dir', type=str, default='/content/output', help='mask_output_dir')
+parser.add_argument('--source_dir', type=str, default='images', help='Directory containing the source images')
+parser.add_argument('--text_output_dir', type=str, default='boundings', help='txt_output_dir')
+parser.add_argument('--mask_output_dir', type=str, default='masks', help='mask_output_dir')
 parser.add_argument('--include_cutout', action='store_true', help='include_cutout')
-parser.add_argument('--cutout_output_dir', type=str, default='/content/output', help='cutout_output_dir')
-parser.add_argument('--delete_txt_files', action='store_true', help='delete_txt_files')
+parser.add_argument('--cutout_output_dir', type=str, default='cutouts', help='cutout_output_dir')
 
-# Load all the text files
-text_files = glob.glob(args.text_output_dir + '/*.txt')
-print('Found {} text files'.format(len(text_files)))
-for t in text_files:
-    # Parse text file to get the bounding boxes
-    with open(t, 'r') as f:
-        lines = f.readlines()
-        # convert each line to a list of floats
-        mask_xyxy = [line.strip().strip('[]').split(', ') for line in lines]
-        print(lines)
-        size = img.size
 
-        img = Image.new("RGB", size, (0, 0, 0))
-        d = ImageDraw.Draw(img)
-        d.rectangle(mask_xyxy, fill="white")
-        # Delete the text file
-        if args.delete_txt_files:
-            os.remove(t)
+def main():
+    args = parser.parse_args()
+    # Create output directories
+    if not os.path.exists(args.mask_output_dir):
+        os.makedirs(args.mask_output_dir)
+    if args.include_cutout and not os.path.exists(args.cutout_output_dir):
+        os.makedirs(args.cutout_output_dir)
+
+    # Load all the text files
+    text_files = glob.glob(args.text_output_dir + '/*.txt')
+    print('Found {} text files'.format(len(text_files)))
+    for t in text_files:
+        # Get the image name
+        name = os.path.basename(t).replace('.txt', '')
+        # Parse text file to get the bounding boxes
+        with open(t, 'r') as f:
+            lines = f.readlines()
+            # convert each line to a list of floats
+            mask_xyxy_list = [list(map(float, line.strip().strip('[]').split(', '))) for line in lines]
+            print(mask_xyxy_list)
+            size = (512, 512)
+
+            for idx, mask_xyxy in enumerate(mask_xyxy_list):
+                # Create mask
+                img = Image.new("RGB", size, (0, 0, 0))
+                d = ImageDraw.Draw(img)
+                d.rectangle(mask_xyxy, fill="white")
+                img.save(args.mask_output_dir + '/m_' + name + '_' + str(idx) + '.jpg')
+
+                # Create cutout
+                if args.include_cutout:
+                    img = Image.open(args.source_dir + '/' + name + '.jpg')
+                    img = img.crop(mask_xyxy)
+                    img.save(args.cutout_output_dir + '/c_' + name + '_' + str(idx) + '.jpg')
+
+
+if __name__ == '__main__':
+    main()
