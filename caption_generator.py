98b48a496e21b4a44c3d8006d9cd53c5cd20b5ba
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-01-31 10:10:19 +0000

    Caption_generator impl

---
+++ caption_generator.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+import sys
+
+import torch
+from PIL import Image
+sys.path.insert(0,'BLIP')
+from models.blip_vqa import blip_vqa
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+
+
+class BlipDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        import glob
+        types = ('*.jpg', '*.png', '*.jpeg')
+        self.img_list = []
+        for files in types:
+            self.img_list.extend(glob.glob(img_dir + '/' + files))
+        print('DATASET CREATED WITH ' + str(len(self.img_list)) + ' files.')
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.img_list[idx]).convert('RGB')
+        path = self.img_list[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, path
+
+    def collate_fn(self, examples):
+
+        pixel_values = [example[0] for example in examples]
+        paths = [example[1] for example in examples]
+
+        pixel_values = torch.stack(pixel_values).to(memory_format=torch.contiguous_format).float()
+
+        batch = {
+            "image_pixel_values": pixel_values,
+            "paths": paths
+        }
+
+        return batch
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image_dir', type=str, default='data/images', help='path to images')
+parser.add_argument('--caption_dir', type=str, default='out')
+
+
+def main():
+    model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_vqa_capfilt_large.pth'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    image_size = 480  # should be kept to 480 for VQA
+    model = blip_vqa(pretrained=model_url, image_size=image_size, vit='base')
+    model.eval()
+    model = model.to(device)
+
+    args = parser.parse_args()
+
+    cutout_folder = args.image_dir
+    caption_folder = args.caption_dir
+
+    if not os.path.exists(caption_folder):
+        os.mkdir(caption_folder)
+
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size), interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    ])
+
+    from torch.utils.data import DataLoader
+    from pathlib import Path
+
+    batch_size = 100
+
+    data = BlipDataset(cutout_folder, transform)
+    inference_dataloader = DataLoader(data, batch_size=batch_size, collate_fn=data.collate_fn)
+
+    data_iter = iter(inference_dataloader)
+
+    question1 = 'what age group is the person?'
+    question2 = 'what is the person wearing?'
+    question3 = 'what is the position of the person?'
+    question4 = 'what is the gender of the person?'
+
+    i = 1
+    for step, batch in enumerate(data_iter):
+        print('STEP: ' + str(step))
+        paths = batch['paths']
+        print(paths)
+        images = batch['image_pixel_values'].to(device)
+        answers = {}
+        with torch.no_grad():
+            for q in [question1, question2, question4, question3]:
+                print('QUESTION: ' + q)
+                answer = model(images, q, train=False, inference='generate')
+                print('ANSWERS: ')
+                print(answer)
+                answers[q] = answer
+
+        i += 1
+
+        for j in range(batch_size):
+            caption = f'{answers[question1][j]} {answers[question4][j]} {answers[question3][j]} wearing {answers[question2][j]}'
+            print(caption)
+            key = Path(paths[j]).stem.replace('c_', 't_', 1)
+            with open(f'{caption_folder}{key}.txt', "w") as text_file:
+                text_file.write(caption)
+
+
+if __name__ == '__main__':
+    main()
--- name_standardise.py
+++ name_standardise.py
@@ -1,22 +1,75 @@
 import os
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', type=str, default='data/images', help='path to images')
+parser.add_argument('--image_dir', type=str, default='data/images', help='path to images')
+parser.add_argument('--mask_dir', type=str, default='out')
+parser.add_argument('--cutout_dir', type=str, default='out')
+parser.add_argument('--boundings_dir', type=str, default='out')
+parser.add_argument('--caption_dir', type=str, default='out')
 parser.add_argument('--prefix', type=str, default='img', help='prefix of images')
-
-
-def group_rename(directory, prefix):
-    for i, f in enumerate(os.listdir(directory)):
-        # Get file format
-        file_ext = f.split('.')[-1]
-        # Rename file
-        os.rename(os.path.join(directory, f), os.path.join(directory, prefix + str(i) + '.' + file_ext))
 
 
 def main():
     args = parser.parse_args()
-    group_rename(args.dir, args.prefix)
+    image_dir = args.image_dir
+    mask_dir = args.mask_dir
+    cutout_dir = args.cutout_dir
+    boundings_dir = args.boundings_dir
+    caption_dir = args.caption_dir
+
+    # Get all the images
+    image_files = glob.glob(image_dir + '/*')
+    print('Found {} image files'.format(len(image_files)))
+
+    # Get all the masks
+    mask_files = glob.glob(mask_dir + '/*')
+    print('Found {} mask files'.format(len(mask_files)))
+
+    # Get all the cutouts
+    cutout_files = glob.glob(cutout_dir + '/*')
+    print('Found {} cutout files'.format(len(cutout_files)))
+
+    # Get all the boundings
+    boundings_files = glob.glob(boundings_dir + '/*')
+    print('Found {} boundings files'.format(len(boundings_files)))
+
+    # Get all the captions
+    caption_files = glob.glob(caption_dir + '/*')
+    print('Found {} caption files'.format(len(caption_files)))
+
+    # Iterate through all the images
+    for idx, image_file in enumerate(image_files):
+        # Get the image name
+        name = os.path.basename(image_file).replace('.jpg', '').replace('.png', '').replace('.jpeg', '')
+        print('Processing {}'.format(name))
+
+        # Get the mask file
+        mask_files = glob.glob(mask_dir + '/m_' + name + '_*')
+        print('Found mask file {}'.format(mask_files))
+
+        # Get the cutout file
+        cutout_files = glob.glob(cutout_dir + '/c_' + name + '_*')
+        print('Found cutout file {}'.format(cutout_files))
+
+        # Get the boundings file
+        boundings_file = glob.glob(boundings_dir + '/' + name)[0]
+        print('Found boundings file {}'.format(boundings_file))
+
+        # Get the caption file
+        caption_files = glob.glob(caption_dir + '/t_' + name + '_*')
+        print('Found caption file {}'.format(len(caption_files)))
+
+        # Rename the files
+        os.rename(image_file, image_file.replace(name, 'img_' + str(idx)))
+        os.rename(boundings_file, boundings_file.replace(name, 'bnd_' + str(idx)))
+        for mask_file in mask_files:
+            os.rename(mask_file, mask_file.replace('m_' + name, 'msk_' + str(idx)))
+        for cutout_file in cutout_files:
+            os.rename(cutout_file, cutout_file.replace('c_' + name, 'cut_' + str(idx)))
+        for caption_file in caption_files:
+            os.rename(caption_file, caption_file.replace('t_' + name, 'cap_' + str(idx)))
 
 
 if __name__ == '__main__':
