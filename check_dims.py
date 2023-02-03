47a7c612a729508ca87997bf4f300d4a7f5cc72e
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-02-03 19:30:09 +0000

    Check_dims changes no input

--- check_dims.py
+++ check_dims.py
@@ -37,50 +37,46 @@ def main():
     print('Found', len(violating_ims), 'violating images')
     print('Would you like to crop {} files?'.format(len(violating_ims)))
     print('This cannot be undone.')
-    answer = input('y/n: ')
-    if answer == 'y':
-        for img_path in violating_ims:
-            # Open image
-            img = Image.open(img_path)
-            print('Image size:', img.size)
-            # Delete image if one dim is smaller than 512
-            if img.size[0] < 512 or img.size[1] < 512:
-                print('Deleting image', img_path)
-                if not dry_run:
-                    os.remove(img_path)
-                continue
-            # Open all corresponding masks
-            basename = re.sub('\.[^/.]+$', '', os.path.basename(img_path))
-            masks = glob.glob(os.path.join(args.mask_dir, 'm_' + basename + '*'))
-            print('Found', len(masks), 'masks for', img_path)
-            # Perform same random crop on image and masks
-            x = random.randint(0, img.size[0] - 512)
-            # x = 0
-            # y = random.randint(0, img.size[1] - 512)
-            y = img.size[1] - 512
-            img = img.crop((x, y, x + 512, y + 512))
+    for img_path in violating_ims:
+        # Open image
+        img = Image.open(img_path)
+        print('Image size:', img.size)
+        # Delete image if one dim is smaller than 512
+        if img.size[0] < 512 or img.size[1] < 512:
+            print('Deleting image', img_path)
+            if not dry_run:
+                os.remove(img_path)
+            continue
+        # Open all corresponding masks
+        basename = re.sub('\.[^/.]+$', '', os.path.basename(img_path))
+        masks = glob.glob(os.path.join(args.mask_dir, 'm_' + basename + '*'))
+        print('Found', len(masks), 'masks for', img_path)
+        # Perform same random crop on image and masks
+        x = random.randint(0, img.size[0] - 512)
+        # x = 0
+        # y = random.randint(0, img.size[1] - 512)
+        y = img.size[1] - 512
+        img = img.crop((x, y, x + 512, y + 512))
 
-            if dry_run:
-                img.show()
-            else:
-                img.save(img_path)
+        if dry_run:
+            img.show()
+        else:
+            img.save(img_path)
 
-            for mask in masks:
-                m = Image.open(mask).convert('L')
-                m = m.crop((x, y, x + 512, y + 512))
-                # Only save mask if it contains at least one white pixel
-                if np.any(np.array(m) == 255):
-                    print('Saving mask', mask)
-                    if dry_run:
-                        m.show()
-                    else:
-                        m.save(mask)
+        for mask in masks:
+            m = Image.open(mask).convert('L')
+            m = m.crop((x, y, x + 512, y + 512))
+            # Only save mask if it contains at least one white pixel
+            if np.any(np.array(m) == 255):
+                print('Saving mask', mask)
+                if dry_run:
+                    m.show()
                 else:
-                    print('Deleting mask', mask)
-                    if not dry_run:
-                        os.remove(mask)
-    else:
-        print('Aborted')
+                    m.save(mask)
+            else:
+                print('Deleting mask', mask)
+                if not dry_run:
+                    os.remove(mask)
 
 
 if __name__ == '__main__':
