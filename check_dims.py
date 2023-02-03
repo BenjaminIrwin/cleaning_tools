3586bf7e91cdb0a44bf8c86b67af5c7745971d6f
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-02-03 12:59:35 +0000

    Check_dims update

--- check_dims.py
+++ check_dims.py
@@ -1,19 +1,26 @@
-# Check if all images in directory are 512x512:
 import argparse
+import glob
 import os
+import random
+import re
 
+import numpy as np
 from PIL import Image
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--source_dir', type=str, default='images', help='Directory containing the source images')
-
+parser.add_argument('--mask_dir', type=str, default='masks', help='Directory containing the source masks')
+parser.add_argument('--dry_run', type=bool, default=True, help='will not delete files if true')
 
 def main():
     args = parser.parse_args()
     source_dir = args.source_dir
+    dry_run = args.dry_run
 
     print('Checking images in', source_dir)
+
+    violating_ims = []
 
     # Iterate through all files in the source directory
     for file in os.listdir(source_dir):
@@ -22,9 +29,47 @@ def main():
             # Try to open the file
             img = Image.open(file_path)
             if img.size != (512, 512):
-                print('Dims violation:', file_path + ' ' + str(img.size))
+                violating_ims.append(file_path)
         except (IOError, SyntaxError) as e:
             print('Bad file:', file_path)
+
+    print('Found', len(violating_ims), 'violating images')
+    print('Would you like to crop {} files?'.format(len(violating_ims)))
+    print('This cannot be undone.')
+    answer = input('y/n: ')
+    if answer == 'y':
+        for img_path in violating_ims:
+            # Open image
+            img = Image.open(img_path)
+            # Open all corresponding masks
+            basename = re.sub('\.[^/.]+$', '', os.path.basename(img_path))
+            masks = glob.glob(os.path.join(args.mask_dir, 'm_' + basename + '*'))
+            print('Found', len(masks), 'masks for', img_path)
+            # Perform same random crop on image and masks
+            x = random.randint(0, img.size[0] - 512)
+            # x = 0
+            # y = random.randint(0, img.size[1] - 512)
+            y = img.size[1] - 512
+            img = img.crop((x, y, x + 512, y + 512))
+
+            if dry_run:
+                img.show()
+            else:
+                img.save(img_path)
+
+
+            for mask in masks:
+                m = Image.open(mask)
+                m = m.crop((x, y, x + 512, y + 512))
+                # Only save mask if it contains at least one white pixel
+                if np.any(np.array(m) == 255):
+                    print('Saving mask', mask)
+                    if dry_run:
+                        m.show()
+                    else:
+                        m.save(mask)
+    else:
+        print('Aborted')
 
 
 if __name__ == '__main__':
