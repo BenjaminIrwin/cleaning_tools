36fdb2a49675fbdba65b96392ee3dbedcd7d9864
kosmicdream <109359031+kosmicdream@users.noreply.github.com>
2023-02-20 16:23:45 +0000

    Update duplicate_remove.py

--- duplicate_remove.py
+++ duplicate_remove.py
@@ -4,14 +4,18 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str, default='data/images', help='path to images')
 parser.add_argument('--work_dir', type=str, default='out', help='path to working directry')
-parser.add_argument('--dry_run', type=bool, default= True, help='set to False to automatically delete duplicates')
+parser.add_argument('--delete_duplicates', action='store_true', help='add argument to delete duplicates at the end')
 
 
 def main():
     args = parser.parse_args()
     work_dir = args.work_dir
     input_dir = args.input_dir
-    dry_run = args.dry_run
+    dry_run = not (args.delete_duplicates)
+
+    if not dry_run:
+        print('You are NOT doing a dry run, all of the duplicates will be deleted at the end')
+        
     fastdup.run(input_dir=input_dir, work_dir=work_dir, nearest_neighbors_k=8,
                 turi_param='ccthreshold=0.96')  # main running function.
     fastdup.create_components_gallery(work_dir, save_path='.')
