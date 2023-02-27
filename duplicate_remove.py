36c46cf39c6e172404d247144543d8f732d006ae
kosmicdream <109359031+kosmicdream@users.noreply.github.com>
2023-02-27 07:58:14 +0000

    Update duplicate_remove.py

--- duplicate_remove.py
+++ duplicate_remove.py
@@ -15,13 +15,13 @@ def main():
 
     if not dry_run:
         print('\n ****You are NOT doing a dry run, all of the duplicates will be deleted at the end!**** \n')
-        
+
     fastdup.run(input_dir=input_dir, work_dir=work_dir, nearest_neighbors_k=8,
                 turi_param='ccthreshold=0.96')  # main running function.
-    fastdup.create_components_gallery(work_dir, save_path='.', num_images=300)
+    fastdup.create_components_gallery(work_dir, save_path='.')
     fastdup.create_duplicates_gallery(work_dir, save_path='.')
     top_components = fastdup.find_top_components(work_dir)
-    
+
     if not dry_run:
         res = fastdup.delete_components(top_components, None, how='one', dry_run=dry_run)
         print('image deleted:')
